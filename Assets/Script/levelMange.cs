c4924626beadc229efe824387183371588d436dc
Robe Zhang <rzhang8277@bths.edu>
2018-11-24 23:05:24 -0500

    Brian Kao!! gitignore

---
+++ Assets/Script/levelMange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelMange : MonoBehaviour {
+
+    public float autoload;
+
+    void Start()
+    {
+        Invoke("LoadNextLevel", autoload);
+    }
+
+	// Use this for initialization
+	public void LoadLevel (string name) {
+        
+        SceneManager.LoadScene(name);
+	}
+	
+	// Update is called once per frame
+	public void SetQuite () {
+        Application.Quit();
+	}
+
+    public void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
---
+++ Assets/Script/player.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class player : MonoBehaviour {
+    [SerializeField] float moveSpeed = 0.5f;
+    // Use this for initialization
+    BoxCollider2D mycollider;
+    Rigidbody2D myRigidbody2D;
+    CapsuleCollider2D mybodycol;
+    Animator myanime;
+    public LayerMask enemyLayer;
+    public LayerMask pitLayer;
+    public int bullets;
+    public GameObject[] numofbullets;
+
+
+    bool isActive = true;
+
+    void Start () {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+        myanime = GetComponent<Animator>();
+        mybodycol = GetComponent<CapsuleCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isActive){ return; }
+        PlayerMove();
+        Die();
+    }
+
+    private void Die()
+    {
+        if (mybodycol.IsTouchingLayers(enemyLayer))
+        {
+
+            //isActive = false;
+            Debug.Log("dead");
+            SceneManager.LoadScene("losemenu");
+            // for(int i = 0; i < numofbullets.Length; i++) {
+            //bullets--;
+            // if(numofbullets.Length >= 0){
+            // Debug.Log(bullets);
+            //Object.Destroy(numofbullets[bullets--]);
+
+            // }else if (numofbullets.Length <= 0)
+            // {
+            // Debug.Log("dead");
+            //SceneManager.LoadScene("losemenu");
+            //}
+            //}
+
+            //}
+        }
+        else if (mybodycol.IsTouchingLayers(pitLayer))
+        {
+            Debug.Log("pit");
+            SceneManager.LoadScene("losemenu");
+        }
+    }
+    private void PlayerMove()
+    {
+        var newXPos = 0f;
+        var newYPos = 0f;
+        // button click 
+        if (CrossPlatformInputManager.GetAxis("Horizontal") != 0 || CrossPlatformInputManager.GetAxis("Vertical") != 0)
+        {
+            Debug.Log("here");
+            newXPos = CrossPlatformInputManager.GetAxis("Horizontal") * moveSpeed;
+            newYPos = CrossPlatformInputManager.GetAxis("Vertical") * moveSpeed;
+        }
+        else // keyboard
+        {
+            newXPos = Input.GetAxis("Horizontal") * moveSpeed;
+            newYPos = Input.GetAxis("Vertical") * moveSpeed;
+        }
+        Vector2 playermoveset = new Vector2(newXPos, newYPos);
+        myRigidbody2D.velocity = playermoveset;
+        
+        myanime.SetFloat("horizontal", newXPos);
+        myanime.SetFloat("vertical", newYPos);
+
+     
+
+
+    }
+   
+}
