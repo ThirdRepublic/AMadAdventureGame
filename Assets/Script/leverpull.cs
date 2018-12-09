b5251f1e8877b75f2bad395b3e9abdd8cbb513c9
billzxy <billzxy1214@hotmail.com>
2018-12-08 21:19:11 -0500

    Changed bullet count representation, photoshopped bullet tile image, added a delay when opening door, added a different collision circle on player for pit detection, widened path in pit areas, fixed player and enemy interaction

--- Assets/Script/leverpull.cs
+++ Assets/Script/leverpull.cs
@@ -1,52 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Threading;
 
-public class leverpull : MonoBehaviour {
+public class leverpull : MonoBehaviour
+{
     BoxCollider2D mycollider;
     Animator myanimator;
     public GameObject[] objectdestroyed;
     public LayerMask playerLayer;
-    
-    // Use this for initialization
-    void Start () {
-        myanimator = GetComponent<Animator>();
 
+    // Use this for initialization
+    void Start()
+    {
+        myanimator = GetComponent<Animator>();
         mycollider = GetComponent<BoxCollider2D>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         pulllever();
 
     }
     private void pulllever()
     {
-        
+
         if (mycollider.IsTouchingLayers(playerLayer))
         {
             //bool isLeverpulled = Input.GetKeyDown(KeyCode.Return);
             //if (isLeverpulled)
             //{
-                myanimator.SetBool("switch", true);
-                
-                for(int i = 0; i < objectdestroyed.Length; i++)
-                {
-                    try
-                    {
-                        Debug.Log(objectdestroyed[i].name);
-                        Object.Destroy(objectdestroyed[i]);
-                    }
-                    catch
-                    {
+            myanimator.SetBool("switch", true);
 
-                    }
-                 }
-           // }
-           
-            
-            
-            
+            for (int i = 0; i < objectdestroyed.Length; i++)
+            {
+                StartCoroutine(destroyAfter1Sec(objectdestroyed[i]));
+                Debug.Log(objectdestroyed[i].name);
+
+            }
+            // }
+
+
+
+
         }
+    }
+    IEnumerator destroyAfter1Sec(GameObject obj)
+    {
+        yield return new WaitForSecondsRealtime(1);
+        UnityEngine.Object.Destroy(obj);
     }
 }
